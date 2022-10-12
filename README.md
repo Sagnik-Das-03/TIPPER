6dcf3e7e2b7adcdda3808417c94c6e62fa6bfedb
Sagnik Das <sagnik07das@gmail.com>
2022-10-12 21:19:25 +0530

    Adding to gitHub

---
+++ README.md
@@ -0,0 +1,46 @@
+# TiPPer (Tip Calculator)
+
+## *Sagnik Das*
+
+**TiPPer** computes the tip and total amount for a bill. The app uses the base amount and tip percentage to calculate the amount owed, and it also describes the quality of service based on the tip.
+
+Time spent: **8** hours spent in total
+
+## Functionality
+
+The following **required** functionality is completed:
+
+* [x ] User can enter in a bill amount (total amount to tip on)
+* [x ] User can enter a tip percentage (what % the user wants to tip).
+* [x ] The tip and total amount are updated immediately when any of the inputs changes.
+* [x ] The user sees a label or color update based on the tip amount.
+
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://gifs.com/gif/walkthrough-792E7A' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+
+## Features
+
+* Pure Kotlin
+* Single Screen
+* No libraries
+
+## License
+
+    Copyright [2022] [Sagnik Das]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ app/src/main/java/com/example/tips/MainActivity.kt
@@ -0,0 +1,183 @@
+/* Extensions
+1.  split the bill by n people
+2. Round up the final amount
+3. Tip Per Person
+4. color or ui updates
+*/
+
+package com.example.tips
+
+import android.animation.ArgbEvaluator
+import android.content.pm.ActivityInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import kotlin.math.roundToInt
+
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  etBaseAmount: EditText
+    private lateinit var seekBarTip: SeekBar
+    private  lateinit var tvTipPercent: TextView
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
+    private lateinit var tvTipDescription: TextView
+    private lateinit var tvTotalRoundedUp: TextView
+    private lateinit var tvBillAmountRounded: TextView
+    private lateinit var tvSplit: TextView
+    private lateinit var etSplit: EditText
+    private lateinit var tvTotalSplit: TextView
+    private  lateinit var tvSplitBillAmount: TextView
+    private lateinit var tvSplitRoundedUp: TextView
+    private lateinit var SplitBillRoundedAmount: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Lock on portrait mode
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT)
+
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        seekBarTip = findViewById(R.id.seekBarTip)
+        tvTipPercent = findViewById(R.id.tvTipPercent)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
+        //new features
+        tvTotalRoundedUp = findViewById(R.id.tvTotalRoundedUp)
+        tvBillAmountRounded = findViewById(R.id.tvBillAmountRounded)
+        tvSplit = findViewById(R.id.tvSplit)
+        etSplit = findViewById(R.id.etSplit)
+        tvTotalSplit = findViewById(R.id.tvTotalSplit)
+        tvSplitBillAmount = findViewById(R.id.tvSplitBillAmount)
+        tvSplitRoundedUp = findViewById(R.id.tvSplitRoundedUp)
+        SplitBillRoundedAmount = findViewById(R.id.SplitBillRoundedAmount)
+
+        seekBarTip.progress = INITIAL_TIP_PERCENT
+        tvTipPercent.text = "$INITIAL_TIP_PERCENT%"
+        updateTipDescription(INITIAL_TIP_PERCENT)
+
+
+        seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG,"onProgressChanged $progress")
+                tvTipPercent.text = "$progress%"
+                computeTipAndTotal()
+                splittingBills()
+                updateTipDescription(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) { }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) { }
+        })
+        etBaseAmount.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.i(TAG,"afterTextChanged $s")
+                computeTipAndTotal()
+            }
+
+        })
+
+        etSplit.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.i(TAG,"afterTextChanges $s")
+                splittingBills()
+            }
+
+        })
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription = when (tipPercent) {
+            in 0..9 -> "Poor"
+            in 10..14 -> "Acceptable"
+            in 15..19 -> "Good"
+            in 20..24 -> "Great"
+            else -> "Amazing"
+        }
+        tvTipDescription.text = tipDescription
+        // Update the color based on tipPercent
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat()/seekBarTip.max,
+            ContextCompat.getColor(this, R.color.red),
+            ContextCompat.getColor(this, R.color.green)
+        ) as Int
+        tvTipDescription.setTextColor(color)
+    }
+
+    private fun computeTipAndTotal(){
+        if(etBaseAmount.text.isEmpty()){
+            tvTipAmount.text = ""
+            tvTotalAmount.text = ""
+            tvBillAmountRounded.text=""
+            return
+        }
+        // 1. Gets the value of base and tip percent
+        val baseAmount = etBaseAmount.text.toString().toDouble()
+        val tipPercent = seekBarTip.progress
+        // 2. Computing the tip and total
+        val tipAmount = baseAmount * tipPercent / 100
+        val totalAmount =  baseAmount + tipAmount
+        var total = String.format("%.02f ₹(INR)", totalAmount)
+        var tip = String.format("%.02f ₹(INR)", tipAmount)
+        val totalRounded = totalAmount.roundToInt()
+        val totalRoundedFormat = String.format("%d ₹(INR)",totalRounded)
+        // 3. Updating the UI
+        tvTipAmount.text = tip
+        tvTotalAmount.text = total
+        tvBillAmountRounded.text = totalRoundedFormat
+    }
+
+    private fun splittingBills(){
+        if(etBaseAmount.text.isEmpty() ){
+            tvTipAmount.text = ""
+            tvTotalAmount.text = ""
+            tvBillAmountRounded.text=""
+            tvSplitRoundedUp.text = ""
+            tvSplitBillAmount.text = ""
+            SplitBillRoundedAmount.text = ""
+            tvSplitBillAmount.text = ""
+            return
+        }
+
+       if(etSplit.text.isEmpty() xor  etBaseAmount.text.isEmpty()){
+            SplitBillRoundedAmount.text = ""
+            tvSplitBillAmount.text = ""
+            return
+        }
+        // 1. Gets the value of base and tip percent and no of persons
+        val baseAmount = etBaseAmount.text.toString().toDouble()
+        val tipPercent = seekBarTip.progress
+        val persons = etSplit.text.toString().toDouble()
+        // 2. Computing the tip and total
+        val tipAmount = baseAmount * tipPercent / 100
+        val totalAmount =  baseAmount + tipAmount
+        val splitBill = totalAmount / persons
+        val splitBillFormat = String.format("%.02f ₹(INR)", splitBill)
+        // spliting logic
+        val splitBillRounded = splitBill.roundToInt()
+        val splitBillRoundedFormat = String.format("%d ₹(INR)",splitBillRounded)
+        // 3.Update the UI
+        tvSplitBillAmount.text = splitBillFormat
+        SplitBillRoundedAmount.text = splitBillRoundedFormat
+    }
+
+
+}
